3fea192322e63803f560b2dff120bacf93cc8c1b
JEAN MATHEUS <jean.almeida@memora.com.br>
2023-02-16 22:57:08 -0300

    Initial commit

---
+++ demo/src/test/java/com/jean/attornatus/AttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.attornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
